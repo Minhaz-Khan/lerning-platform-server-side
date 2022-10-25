903e486b4e30459c04979e96ad496a0d42bd1523
Minhaz-Khan <kminhaz111@gmail.com>
2022-10-25 12:23:22 +0600

    all data collection done

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+const courses = require('./courses.json');
+const couresDetails = require('./couresItem.json')
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/coures/:id', (req, res) => {
+    const id = req.params.id;
+    const couresItem = couresDetails.find(course => course._id == id);
+    res.send(couresItem)
+
 })
 app.listen(port, () => {
     console.log(`example app listening on prot${port}`);
